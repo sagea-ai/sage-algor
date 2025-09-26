79f85c62a4599ad08759ac6f04af6012abd5a00f
comethrusws <basabjha@naver.com>
2025-09-26 15:18:46 +0545

    add: command handlers

--- src/commands/handler.js
+++ src/commands/handler.js
@@ -7,16 +7,28 @@ export const handleCommand = (line, outputBox, showError) => {
     const args = parts.slice(1);
 
     switch (command) {
-        case ':edit':
+        case '/help':
+            const helpMessage = `
+Available commands:
+/help                - Display this help message.
+/edit <file>         - Load a file into the context (Not implemented).
+/run <command>       - Run a command (Not implemented).
+/test <file>         - Run tests for a file (Not implemented).
+/history             - Display command history (Not implemented).
+exit, quit           - Exit the application.
+`;
+            outputBox.setContent(helpMessage);
+            break;
+        case '/edit':
             outputBox.setContent(chalk.blue(`Loading ${args.join(' ')} into context... (Not implemented)`));
             break;
-        case ':run':
+        case '/run':
             outputBox.setContent(chalk.blue(`Running ${args.join(' ')}... (Not implemented)`));
             break;
-        case ':test':
+        case '/test':
             outputBox.setContent(chalk.blue(`Testing ${args.join(' ')}... (Not implemented)`));
             break;
-        case ':history':
+        case '/history':
             outputBox.setContent(chalk.blue('Displaying history... (Not implemented)'));
             break;
         case 'exit':
